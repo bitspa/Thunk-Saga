8ad134294d9ef74082e2bf13197559148845a765
Spandita <spandita.banerjee@bitcanny.com>
2022-12-19 17:31:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux"
+import { userAction } from './Components/userActions'
+import { User } from "./Components/user"
 
 function App() {
+  const dispatch = useDispatch()
+  const userList = useSelector(state => state.userList)
+  const { loading, users, error } = userList
+
+  useEffect(() => {
+    dispatch(userAction())
+  }, [dispatch])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Redux</h2>
+
+
+      {loading ? <h2>loading...</h2> : error ? <h2>{error}</h2> :
+        <User users={users} />}
+    </>
   );
 }
 
 export default App;
+
